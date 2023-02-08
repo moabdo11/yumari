748c425717aefb989ed0fa4e6aa72037d0efa07e
Mo Abdo <moabdo@Mos-MacBook-Air.lan>
2023-02-08 16:33:18 -0500

    added components folder and mantine dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MantineProvider, Text, Button, Paper, Loader } from '@mantine/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS 
+      theme={{
+        components: {
+          Text: {
+            styles: {
+              root: { 
+                fontSize: 20,
+                fontFamily: "Open Sans",
+              },
+            },
+          },
+        },
+        colorScheme: "dark",
+        loader: 'bars',
+        colors: {
+          blue:[
+            "#FF6B6B", 
+            "#BE4BDB", 
+            "#15AABF", 
+            "#82C91E", 
+            "#FFD43B",
+            "#E8590C", 
+            "#A5D8FF", 
+            "#5F3DC4", 
+            "#099268", 
+            "#66D9E8"
+          ,]
+        },
+      }}
+    
+    >
+      
+      <Paper>
+        <Text>Welcome to Mantine!</Text>
+        <Button>Hello Mo!</Button>
+        <Button>Hello Mo!</Button>
+        <Loader />
+      </Paper>
+    </MantineProvider>
   );
 }
 
---
+++ src/components/BuyerSection/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BuyerSection(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BuyerSection;
